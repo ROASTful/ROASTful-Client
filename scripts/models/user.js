b55cf5ba9d2071edc5b949ee21426907f77bd075
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-15 11:56:20 -0800

    puts should work for user

---
+++ scripts/models/user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = app || {};
+var __API_URL__ = 'https://roastful.herokuapp.com';
+// var __API_URL__ = 'http://localhost:3000';
+
+function User(rawDataObj) {
+  Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
+}
+
+User.update= (user) =>
+$.ajax({
+      url: `${__API_URL__}/api/v1/users/${user.username}`,
+      method: 'PUT',
+      data: user
+    })
+      .then(() => {console.log('updated'); page('/')})
+      .catch(errorCallback);
+  };
