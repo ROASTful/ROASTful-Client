0aee519410f56180a62b7fcc5567181aec2bd162
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-15 11:27:20 -0800

    actually worked on routes and inits and search functionality

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,12 +2,13 @@
 var app = app || {};
 
 
-page('/', ctx => app.Ingredient.fetchAll(app.ingredientView.initIndexPage));
+page('/', ctx => app.ingredientView.initIndexPage);
 page('/ingredient/error', ctx => app.ingredientView.errorCallback(ctx));
-page('/pantry', '/');
-page('/recipe', '/');
-
-page('/pantry/:pantryName', app.ingredientController.loadByPantry, app.ingredientController.index);
-page('/recipe/:recipeName', app.recipeController.loadByRecipe, app.ingredientController.index);
+page('/user/pantry', app.ingredientView.initPantryPage);
+page('/user/recipes', app.ingredientView.initRecipePage);
+// page('/recipe', '/');
+//
+// page('/pantry/:pantryName', app.ingredientController.loadByPantry, app.ingredientController.index);
+// page('/recipe/:recipeName', app.recipeController.loadByRecipe, app.ingredientController.index);
 
 page();
