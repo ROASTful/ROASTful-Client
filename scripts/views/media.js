94dc2cb48017ba86451c17065ef6cae2b7dd8b67
Snobeard <fatmattmatt@gmail.com>
2017-11-17 09:09:23 -0800

    pushing up media files

---
+++ scripts/views/media.js
@@ -0,0 +1,29 @@
+'use strict';
+
+$('.icon-menu').click(function() {
+  $('.main-nav ul').slideToggle();
+})
+
+$('.main-nav ul li').click(function() {
+  if ($(window).width() < 709) {
+    $('.main-nav ul').hide();
+  }
+})
+
+// resets nav bar after window resize
+$(window).resize(function() {
+  if ($(window).width() > 709) {
+    $('.main-nav ul').fadeIn();
+  } else if ($(window).width() < 709) {
+    $('.main-nav ul').hide();
+  }
+})
+
+$('html').click(function() {
+  $('#login').hide();
+});
+
+// prevents click event functionality
+$('#login').click(function(event){
+    event.stopPropagation();
+});
