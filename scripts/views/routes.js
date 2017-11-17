fbc81b6f94d99fbff4a170bb8fd403c77acbfc09
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-16 17:37:21 -0800

    removed pantry route

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -6,7 +6,6 @@ page('/', app.ingredientView.initIndexPage);
 page('/ingredient/error', ctx => app.ingredientView.errorCallback(ctx));
 page('/user/about', app.ingredientView.initAboutPage);
 page('/user/recipes', app.ingredientView.initRecipePage);
-page('/user/pantry', app.ingredientView.initPantryPage);
 page('/user/login', app.ingredientView.initLoginPage);
 
 page();
